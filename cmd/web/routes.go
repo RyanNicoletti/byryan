6c6e6df5f9c39d802240f221476a7678886c8073
Ryan Nicoletti <r.nicoletti1@gmail.com>
2025-06-23 19:36:31 -0400

    add comment form

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,6 +6,25 @@ import (
 	"byryan.net/config"
 )
 
+type MiddlewareChain struct {
+	middlewares []func(http.Handler) http.Handler
+}
+
+func (mc MiddlewareChain) Then(h http.Handler) http.Handler {
+	return middlewareChain(h, mc.middlewares...)
+}
+
+func NewChain(middlewares ...func(http.Handler) http.Handler) MiddlewareChain {
+	return MiddlewareChain{middlewares: middlewares}
+}
+
+func middlewareChain(h http.Handler, middlewares ...func(http.Handler) http.Handler) http.Handler {
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		h = middlewares[i](h)
+	}
+	return h
+}
+
 func routes(app *config.Application) http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -17,5 +36,10 @@ func routes(app *config.Application) http.Handler {
 	mux.Handle("POST /comment/create", createComment(app))
 	mux.Handle("GET /about", about(app))
 	mux.Handle("GET /arcade", arcade(app))
-	return recoverPanic(logRequest(commonHeaders(mux), app), app)
+
+	standard := NewChain(func(h http.Handler) http.Handler { return recoverPanic(h, app) },
+		func(h http.Handler) http.Handler { return logRequest(h, app) },
+		func(h http.Handler) http.Handler { return commonHeaders(h) })
+
+	return standard.Then(mux)
 }
