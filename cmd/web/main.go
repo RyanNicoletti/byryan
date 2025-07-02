5d38d6f1c50a366bf3281e79fe09c36fe72efa8c
Ryan Nicoletti <r.nicoletti1@gmail.com>
2025-07-02 07:33:59 -0400

    https server configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"database/sql"
 	"log/slog"
 	"net/http"
@@ -36,10 +37,29 @@ func main() {
 
 	app := config.NewApplication(logger, db, templateCache)
 
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+		// default ciphers for go's http server but with the CBC ciphers omitted
+		// TLS 1.3 ciphers are ommitted as they will be used by default if TLS 1.3 is negotiated
+		// ref: https://github.com/golang/go/issues/13385
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		},
+	}
+
 	srv := &http.Server{
-		Addr:     cfg.Addr,
-		Handler:  routes(app),
-		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		Addr:         cfg.Addr,
+		Handler:      routes(app),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("starting server", slog.String("addr", cfg.Addr))
